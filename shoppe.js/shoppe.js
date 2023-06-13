f84165bd6d482f8c5be87b6fe183b5f387290026
hungdo161 <dongochung.161@gmail.com>
2023-06-13 23:56:33 +0700

    login and register javascript coding

--- shoppe.js/shoppe.js
+++ shoppe.js/shoppe.js
@@ -1,14 +1,64 @@
 
+// register modal
 
-
-const modalElement = document.querySelector(".modal")
+const registerModalElement = document.querySelector(".js-register_modal-wraper")
 const registerElement = document.querySelector(".js-header_nav-support-register");
 const registerModal = document.querySelector(".js-register_modal");
+const closeRegisterModal = document.querySelector(".js-register-confirm-back");
 
+// login modal
 
-function showLoginModal () {
-    modalElement.classList.add("open");
+const loginModalElement = document.querySelector(".js-login_modal-wraper")
+const loginElement = document.querySelector(".header_nav-support-login");
+const loginModal = document.querySelector(".js-login_modal");
+const closeLoginModal = document.querySelector(".js-login-confirm-back")
+
+// show register modal
+registerElement.addEventListener("click",function() {
+    registerModalElement.classList.add("open");
     registerModal.classList.add("open");
-}
+});
 
-registerElement.addEventListener("click",showLoginModal);
+// close register modal
+
+closeRegisterModal.addEventListener("click",function() {
+    registerModalElement.classList.remove("open");
+    registerModal.classList.remove("open");
+});
+
+
+// show login modal
+
+loginElement.addEventListener("click",function() {
+    loginModalElement.classList.add("open");
+    loginModal.classList.add("open");
+});
+
+// close login modal
+
+closeLoginModal.addEventListener("click",function () {
+    loginModalElement.classList.remove("open");
+    loginModal.classList.remove("open");
+});
+
+
+// login modal and register modal convert
+
+// convert register to login
+const convertLoginBtn = document.querySelector(".js-register_modal-login");
+
+convertLoginBtn.addEventListener("click",function() {
+    registerModalElement.classList.remove("open");
+    loginModalElement.classList.add("open");
+    loginModal.classList.add("open");
+});
+
+// convert login to register
+
+const convertRegisterBtn = document.querySelector(".js-login_modal-register");
+
+convertRegisterBtn.addEventListener("click",function() {
+    loginModalElement.classList.remove("open");
+    registerModalElement.classList.add("open");
+    registerModal.classList.add("open");
+});
