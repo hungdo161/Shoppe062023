95551945e43e724ebc4c3e440d438248f6277826
hungdo161 <hung.do@pirago.work>
2023-06-13 17:24:15 +0700

    JS code

--- shoppe.js/shoppe.js
+++ shoppe.js/shoppe.js
@@ -1,2 +1,14 @@
 
 
+
+const modalElement = document.querySelector(".modal")
+const registerElement = document.querySelector(".js-header_nav-support-register");
+const registerModal = document.querySelector(".js-register_modal");
+
+
+function showLoginModal () {
+    modalElement.classList.add("open");
+    registerModal.classList.add("open");
+}
+
+registerElement.addEventListener("click",showLoginModal);
